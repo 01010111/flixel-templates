52e4040ecc422f413cf964479e8e115bda82183e
Will Blanton <will@Wills-MacBook-Pro.local>
2019-09-27 12:41:47 -0400

    :sparkles:

--- default/source/Main.hx
+++ default/source/Main.hx
@@ -2,12 +2,23 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+#if PIXEL_PERFECT
+import flixel.FlxG;
+import flixel.system.FlxAssets.FlxShader;
+import openfl.display.StageQuality;
+import openfl.filters.ShaderFilter;
+#end
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, states.PlayState, 1, 60, 60, true));
+		#if PIXEL_PERFECT
+		FlxG.game.setFilters([new ShaderFilter(new FlxShader())]);
+		FlxG.game.stage.quality = StageQuality.LOW;
+		FlxG.resizeWindow(640, 360);
+		#end
 	}
 }
