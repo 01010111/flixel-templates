5eccb2918232851ea9f6a26587ee5e3a309b9ed9
Will Blanton <will@Wills-MBP.fios-router.home>
2019-12-22 12:13:50 -0500

    added some eventbus stuff, tweaked sizes

--- default/source/Main.hx
+++ default/source/Main.hx
@@ -1,7 +1,11 @@
 package;
 
-import flixel.FlxGame;
 import openfl.display.Sprite;
+import flixel.FlxGame;
+import zero.utilities.ECS;
+import zero.utilities.Timer;
+import zero.utilities.SyncedSin;
+import zero.flixel.input.FamiController;
 #if PIXEL_PERFECT
 import flixel.FlxG;
 import flixel.system.FlxAssets.FlxShader;
@@ -11,14 +15,23 @@ import openfl.filters.ShaderFilter;
 
 class Main extends Sprite
 {
+	static var WIDTH:Int = 0;
+	static var HEIGHT:Int = 0;
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, states.PlayState, 1, 60, 60, true));
+		addChild(new FlxGame(WIDTH, HEIGHT, states.PlayState, 1, 60, 60, true));
+		((?dt) -> {
+			ECS.tick(dt);
+			Timer.update(dt);
+			SyncedSin.update(dt);
+			FamiController.update(dt);
+		}).listen('preupdate');
 		#if PIXEL_PERFECT
 		FlxG.game.setFilters([new ShaderFilter(new FlxShader())]);
 		FlxG.game.stage.quality = StageQuality.LOW;
-		FlxG.resizeWindow(640, 360);
+		FlxG.resizeWindow(FlxG.stage.stageWidth, FlxG.stage.stageHeight);
 		#end
 	}
 }
